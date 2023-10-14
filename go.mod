534a615e9054501a42e10f54490dd4c5b915f39f
髙橋一成 <kazunari@takahashiisseinoMacBook-Air.local>
2023-10-14 23:14:57 +0900

    Complete

--- go.mod
+++ go.mod
@@ -3,11 +3,9 @@ module todo_app
 go 1.20
 
 require (
+	github.com/google/uuid v1.3.0
 	github.com/mattn/go-sqlite3 v1.14.17
 	gopkg.in/ini.v1 v1.67.0
 )
 
-require (
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
-)
+require github.com/stretchr/testify v1.8.4 // indirect
--- main.go
+++ main.go
@@ -2,24 +2,13 @@ package main
 
 import (
 	"fmt"
+	"todo_app/app/controllers"
 	"todo_app/app/models"
 )
 
 func main() {
-	// fmt.Println(config.Config.Port)
-	// fmt.Println(config.Config.SQLDriver)
-	// fmt.Println(config.Config.DbName)
-	// fmt.Println(config.Config.LogFile)
-
-	// log.Println("test")
-
 	fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.Password = "testtest"
-	fmt.Println(u)
+	controllers.StartMainServer()
 
-	u.CreateUser()
 }
